81247a1b912c50285e6e9c90087b69ee6a8315b3
Florian MONBEIG <fmonbeig@e1r6p15.42.fr>
2022-07-05 16:38:47 +0200

    installation de mariaDB ok, WP en cours

--- srcs/requirements/mariadb/conf/ini.sh
+++ srcs/requirements/mariadb/conf/ini.sh
@@ -1,15 +1,41 @@
 #!/bin/sh
 
-set -x #for more information in terminal
+#set -e #Exit if we have a non-zero after a CMD
+#set -x #for more information in terminal(debugg)
 
-#Myconf.cnf have already been configured
+#First we have to launch mysql then we sleep until the server is available
+#The command "mysqladmin ping" return 0 when the server is up
+#https://dev.mysql.com/doc/refman/8.0/en/mysqladmin.html
+
+#------------WAITING FOR SERVER------------#
+service mysql start
+while ! mysqladmin ping; do
+	sleep 2
+done
+
 #we will enter the mysql to configurate user and create the Wordpress database
+#------------CREATE DATABASE------------#
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+
+#------------CREATE USER------------#
+#1 - ADMIN
 mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
 mysql -u root -e "GRANT ALL ON *.* TO '${MYSQL_ADMIN}'@'%';"
+#2 - USER (login42)
 mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_USERPASSWORD}';"
 mysql -u root -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;" #reload all user rules
-killall mysqld						# kill mysqld
 
-mysqld #launch mysql server
+#------------DELETE USER------------#
+#We don't need root anymore because we have ADMIN with all right
+mysql -u root -e "DELETE FROM mysql.user WHERE user='';"
+mysql -u root -e "DELETE FROM mysql.user WHERE user='root';"
+
+#------------RELOAD ALL USERS RULES------------#
+mysql -u root -e "FLUSH PRIVILEGES;" #reload all user rules
+
+#------------RELOAD MYSQL------------#
+killall mysqld
+mysqld
+
+#mysql -u root -e
+#stand for mysql --user root --execute
--- srcs/requirements/wordpress/conf/ini.sh
+++ srcs/requirements/wordpress/conf/ini.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-set -x #for more information in terminal
+#set -e #Exit if we have a non-zero after a CMD
+#set -x #for more information in terminal(debugg)
 
 #We use the wp cli to create the wp-config.php and config our wordpress
+# https://wp-cli.org/fr/
+# https://developer.wordpress.org/cli/commands/
+
+#------------INSTALL WORDPRESS------------#
+# the famous 5 minute install in 0.0001s
 wp core install \
 --allow-root \ # you can do command even if you are root
 --path=/var/www/html \
@@ -12,6 +18,8 @@ wp core install \
 --admin_password=${WORDPRESS_ADMIN_PASSWORD} \
 --admin_email=${WORDPRESS_ADMIN_MAIL}
 
+#------------CONFIG FILE------------#
+#creation of the wp-config.php
 wp config create \
 --allow-root \
 --dbname=${MYSQL_DATABASE}
@@ -20,17 +28,19 @@ wp config create \
 --dbhost=mariadb:3306 \
 --dbcollate="utf8_unicode_520_ci"  # The ultimate Unicode set of rules
 
+#------------USER CREATION------------#
+# After Admin we create the user "login42"
 wp user create ${WORDPRESS_USER} ${WORDPRESS_USER_MAIL} \
 --allow-root \
 --path=/var/www/html \
 --first_name=florian \
 --last_name=MONBEIG \
---descriton=this user is very cool \
+--description=this user is very cool \
 --role=author \
 --user_pass=${WORDPRESS_USER_PASSWORD}
 
+mysqld
 
-mysqld #launch mysql server
+# php-fpm #launch mysql server
 
 # --allow-root
-
